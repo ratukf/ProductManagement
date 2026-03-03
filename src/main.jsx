067a3250b8412ac7b6da91c2a0b6819f700eef8c
Ratu <ratukf@gmail.com>
2026-03-04 01:43:44 +0700

    chore(route): add new route (add & edit product)

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import ProtectedRoute from "./components/layout/ProtectedRoute.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ProductDetailPage from "./pages/ProductDetailPage.jsx";
 import ProductsPage from "./pages/ProductsPage.jsx";
+import AddProductPage from "./pages/AddProductPage.jsx";
+import EditProductPage from "./pages/EditProductPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -15,6 +17,8 @@ createRoot(document.getElementById("root")).render(
         <Route path="/dashboard" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/products/:id/edit" element={<EditProductPage />} />
+        <Route path="/products/add" element={<AddProductPage />} />
       </Route>
     </Routes>
   </BrowserRouter>,
