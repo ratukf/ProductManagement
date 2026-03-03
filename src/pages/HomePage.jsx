db52172220d31ffb3bbf854197d7598cc8fc635e
Ratu <ratukf@gmail.com>
2026-03-03 22:39:37 +0700

    chore: add log out button to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useAuthStore } from "../store/authStore";
+import { useLogout } from "../hooks/useLogout";
 
 const HomePage = () => {
   const { user } = useAuthStore();
+  const { handleLogout } = useLogout();
+
   return (
     <Box>
       <Typography variant="h4">
-        Hello, {user.firstName} {user.lastName}
+        Hello, {user?.firstName} {user?.lastName}
       </Typography>
+      <Button onClick={handleLogout}>Logout</Button>
     </Box>
   );
 };
