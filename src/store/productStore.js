96e59a9c308be909efac0e182fdc8739c12510f1
Ratu <ratukf@gmail.com>
2026-03-04 00:25:55 +0700

    feat(store): add get one product by id

--- src/store/productStore.js
+++ src/store/productStore.js
@@ -3,11 +3,13 @@ import { create } from "zustand";
 const useProductStore = create((set) => ({
   // Product state
   products: [],
+  currentProduct: null,
   isLoading: false,
   error: null,
 
   // Product state setter
   setProducts: (products) => set({ products }),
+  setCurrentProduct: (currentProduct) => set({ currentProduct }),
   setLoading: (isLoading) => set({ isLoading }),
   setError: (isError) => set({ isError }),
 }));
