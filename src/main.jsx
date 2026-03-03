3397af084adcb5236e4632d88e7adda51e1cd8be
Ratu <ratukf@gmail.com>
2026-03-04 03:13:46 +0700

    chore: wrap main.jsx in themeprovider and cssbaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme.js";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+);
