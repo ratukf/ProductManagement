8abb739361c56760caeb50b2ba04c74173178323
Ratu <ratukf@gmail.com>
2026-03-04 01:43:00 +0700

    chore(page): add delete function to product detail page

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -7,19 +7,27 @@ import {
 } from "@mui/material";
 import { useProductStore } from "../store/productStore";
 import { useParams, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { getProductById } from "../action/productAction";
+import { useEffect, useState } from "react";
+import { deleteProduct, getProductById } from "../action/productAction";
 import { DeleteOutline, Edit } from "@mui/icons-material";
+import { ConfirmDialog } from "../components/ConfirmDialog";
 
 const ProductDetailPage = () => {
-  const { currentProduct, isLoading } = useProductStore();
+  const { currentProduct, isLoading, isSubmitting } = useProductStore();
   const { id } = useParams();
   const nav = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Fetch product by id
   useEffect(() => {
     if (id) getProductById({ id });
   }, [id]);
+
+  // Handle delete product
+  const handleDelete = async () => {
+    const result = await deleteProduct({ id: id });
+    if (result.success) nav("/products");
+  };
 
   // Edge case if loading or product is empty
   if (isLoading) return <CircularProgress />;
@@ -38,7 +46,7 @@ const ProductDetailPage = () => {
       >
         Edit
       </Button>
-      <IconButton>
+      <IconButton onClick={() => setIsModalOpen(true)}>
         <DeleteOutline />
       </IconButton>
 
@@ -52,6 +60,16 @@ const ProductDetailPage = () => {
       <Typography variant="body1">Stock: {currentProduct.stock}</Typography>
       <Typography variant="body1">Rating: {currentProduct.rating}</Typography>
       <Typography variant="body1">{currentProduct.description}</Typography>
+
+      {/* Delete confirm modal */}
+      <ConfirmDialog
+        open={isModalOpen}
+        title="Delete Product"
+        description={`Are you sure you want to delete "${currentProduct?.title}"?`}
+        onConfirm={handleDelete}
+        onCancel={() => setIsModalOpen(false)}
+        isLoading={isSubmitting}
+      />
     </Box>
   );
 };
