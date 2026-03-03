7c75bccf1bbc4fae1bf6022f27007355f6309d94
Ratu <ratukf@gmail.com>
2026-03-04 00:54:51 +0700

    feat(component): add confirm dialog modal

---
+++ src/components/ConfirmDialog.jsx
@@ -0,0 +1,41 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+
+const ConfirmDialog = ({
+  open,
+  title,
+  description,
+  onConfirm,
+  onCancel,
+  isLoading,
+}) => {
+  return (
+    <Dialog open={open} onClose={onCancel}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{description}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onCancel} disabled={isLoading}>
+          Cancel
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={onConfirm}
+          disabled={isLoading}
+        >
+          {isLoading ? "Deleting..." : "Delete"}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export { ConfirmDialog };
