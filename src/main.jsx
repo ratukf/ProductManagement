28a0a8ed4d22e5d2e38ab8239f748c66bbfc0459
Ratu <ratukf@gmail.com>
2026-03-03 22:12:26 +0700

    chore: wrap dashboard inside protected route

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
+import ProtectedRoute from "./components/layout/ProtectedRoute.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
